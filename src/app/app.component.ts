c4b99ab88a90e060a2f94ceb885ebd571d1e3661
shanegibney <shanegibney@gmail.com>
2017-04-19 18:25:18 +0100

    current location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+  location = {};
   title: string = 'This... is rye first angular2-google-maps project';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  // lat: number = 51.678418;
+  // lng: number = 7.809007;
+  lat: number = null;
+  lng: number = null;
+
+  // lat: number = position.coords.latitude;
+  // lat: number = position.coords.latitude;
+  // lng: number = position.coords.longitude;
+
+  setPosition(position){
+      this.location = position.coords;
+        // console.log(position.coords.latitude);
+        // console.log(position.coords.longitude);
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+      }
 
   ngOnInit(){
-    console.log("booee!");
+    // console.log("latitude: " + location);
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(this.setPosition.bind(this));
+      };
+
+   }
   }
-}
